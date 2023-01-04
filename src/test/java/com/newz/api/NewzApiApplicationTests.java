071cbf662f3e078db21bdc928372f43d3d3d484c
Kate <kate@icaremind.com>
2023-01-04 11:56:41 +0900

    Initialize Project

---
+++ src/test/java/com/newz/api/NewzApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newz.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewzApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
