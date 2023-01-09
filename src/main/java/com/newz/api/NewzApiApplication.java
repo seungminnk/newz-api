98800491817d670cfc8526925d547d0fb233f21d
sggnology <sggnology@gmail.com>
2023-01-09 19:04:24 +0900

    [document] 서버 설명 및 호스트 리스트에 실서버 도메인 추가

--- src/main/java/com/newz/api/NewzApiApplication.java
+++ src/main/java/com/newz/api/NewzApiApplication.java
@@ -1,9 +1,23 @@
 package com.newz.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+	info = @Info(
+			title = "Newz api 서버",
+			version = "v1",
+			description = "Newz api 서버 문서입니다."
+	),
+	servers = {
+		@Server(url = "localhost:3001", description = "로컬 서버"),
+		@Server(url = "https://newz.bbear.kr", description = "메인 서버"),
+	}
+)
 @SpringBootApplication
 @MapperScan(annotationClass = org.apache.ibatis.annotations.Mapper.class)
 public class NewzApiApplication {
