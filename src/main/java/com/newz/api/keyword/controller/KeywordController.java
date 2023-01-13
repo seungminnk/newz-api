50383dbe713ba0d2277047631dd78a7aa0c66606
Kate Kwak <minimani.juice@gmail.com>
2023-01-14 00:44:57 +0900

    Feat: Swagger를 위한 코드 추가

--- src/main/java/com/newz/api/keyword/controller/KeywordController.java
+++ src/main/java/com/newz/api/keyword/controller/KeywordController.java
@@ -1,6 +1,11 @@
 package com.newz.api.keyword.controller;
 
 import com.newz.api.keyword.service.KeywordService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +23,19 @@ public class KeywordController {
 
   private KeywordService keywordService;
 
+  @Operation(summary = "온보딩 단계에서 하단에 보여줄 예시(고정) 키워드 목록 가져오기")
+  @ApiResponses(value = {
+      @ApiResponse(
+          responseCode = "200",
+          description = "9개씩 랜덤한 순서로 반환됨",
+          content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = ArrayList.class)
+              )
+          }
+      )
+  })
   @GetMapping("/fixed-keyword/list")
   public ResponseEntity<List<String>> getFixedKeywordList() {
     return new ResponseEntity<>(keywordService.getFixedKeywords(), HttpStatus.OK);
--- src/main/java/com/newz/api/user/controller/UserController.java
+++ src/main/java/com/newz/api/user/controller/UserController.java
@@ -1,10 +1,19 @@
 package com.newz.api.user.controller;
 
+import com.newz.api.common.exception.ErrorResponse;
+import com.newz.api.news.keywordnews.model.NewsListResponse;
 import com.newz.api.user.model.BookmarkAddRequest;
 import com.newz.api.user.model.BookmarkNewsListResponse;
 import com.newz.api.user.model.UserInformationResponse;
 import com.newz.api.user.model.UserKeywordSetRequest;
 import com.newz.api.user.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 import java.util.Map;
 import javax.validation.Valid;
@@ -26,16 +35,63 @@ public class UserController {
 
   private UserService userService;
 
+  @Operation(summary = "사용자 정보 가져오기")
+  @ApiResponses(value = {
+      @ApiResponse(
+          responseCode = "200",
+          description = "넘겨준 userId에 해당하는 사용자 정보 반환",
+          content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = NewsListResponse.class)
+              )
+          }
+      ),
+      @ApiResponse(
+          responseCode = "400",
+          description = "넘겨준 userId에 해당하는 사용자가 존재하지 않을 경우 400(Not Found) 반환",
+          content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = ErrorResponse.class)
+              )
+          }
+      )
+  })
   @GetMapping("")
   public ResponseEntity<UserInformationResponse> getUserInformation(@RequestParam("userId") int userId) {
     return new ResponseEntity<>(userService.getUserInformationByUserId(userId), HttpStatus.OK);
   }
 
+  @Operation(summary = "사용자가 등록한 키워드 목록 가져오기")
+  @ApiResponses(value = {
+      @ApiResponse(
+          responseCode = "200",
+          description = "넘겨준 userId에 해당하는 사용자가 등록해 둔 키워드 목록 반환 (List<String>)",
+          content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = List.class)
+              )
+          }
+      )
+  })
   @GetMapping("/keyword/list")
   public ResponseEntity<List<String>> getUserKeywordList(@RequestParam("userId") int userId) {
     return new ResponseEntity<>(userService.getUserKeywordsByUserId(userId), HttpStatus.OK);
   }
 
+  @Operation(summary = "키워드 등록하기")
+  @ApiResponses(value = {
+      @ApiResponse(
+          responseCode = "200",
+          description = "원하는 키워드를 등록(키워드 여러개 한꺼번에 등록 가능하지만, 총 9개까지 등록 가능.)"
+      ),
+      @ApiResponse(
+          responseCode = "409",
+          description = "등록하려는 키워드 개수와 등록된 키워드 개수를 합해 9개를 초과한 경우 409(Conflict) 반환"
+      )
+  })
   @PostMapping("/keyword")
   public ResponseEntity<List<Map<String, Object>>> setUserKeyword(@RequestBody @Valid UserKeywordSetRequest request)
       throws Exception {
@@ -43,6 +99,19 @@ public class UserController {
     return new ResponseEntity<>(HttpStatus.OK);
   }
 
+  @Operation(summary = "사용자가 북마크해둔 뉴스 기사 데이터 반환")
+  @ApiResponses(value = {
+      @ApiResponse(
+          responseCode = "200",
+          description = "넘겨준 userId에 해당하는 사용자가 북마크해둔 뉴스 기사 목록 반환",
+          content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = BookmarkNewsListResponse.class)
+              )
+          }
+      )
+  })
   @GetMapping("/bookmark/news")
   public ResponseEntity<BookmarkNewsListResponse> getUserBookmarkNews(
       @RequestParam("userId") int userId,
@@ -51,12 +120,26 @@ public class UserController {
     return new ResponseEntity<>(userService.getUserBookmarkNewsByUserId(userId, page, limit), HttpStatus.OK);
   }
 
+  @Operation(summary = "북마크 추가")
+  @ApiResponses(value = {
+      @ApiResponse(
+          responseCode = "201",
+          description = "북마크 추가에 성공하면 201(Created) 반환"
+      )
+  })
   @PostMapping("/bookmark/add")
   public ResponseEntity<Void> addBookmark(@RequestBody @Valid BookmarkAddRequest request) {
     userService.addUserBookmark(request);
     return new ResponseEntity<>(HttpStatus.CREATED);
   }
 
+  @Operation(summary = "북마크 제거")
+  @ApiResponses(value = {
+      @ApiResponse(
+          responseCode = "200",
+          description = "북마크 제거에 성공하면 200(OK) 반환"
+      )
+  })
   @PatchMapping("/bookmark/remove")
   public ResponseEntity<Void> removeBookmark(@RequestParam("bookmarkId") int bookmarkId) {
     userService.removeUserBookmark(bookmarkId);
